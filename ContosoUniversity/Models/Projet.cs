293e62248ccbe07068b623ee8ba8983e72d82405
MoujanniMehdi <moujannimehdi@gmail.com>
2021-05-29 12:34:00 +0100

    changes to html Views

--- ContosoUniversity/Models/Projet.cs
+++ ContosoUniversity/Models/Projet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,13 @@ namespace ContosoUniversity.Models
     public class Projet
     {
         public int ID { get; set; }
+        [Display(Name = "Project Name")]
         public string nameProjet { get; set; }
+        [Display(Name = "Project Theme")]
         public string theme { get; set; }
+        [Display(Name = "Project Description")]
         public string description { get; set; }
+        [Display(Name = "Instructor ID")]
         public Instructor Encadrant { get; set; }
         public ICollection<Student> Binome { get; set; }
 
